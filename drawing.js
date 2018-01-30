7098764cdc2809e69d7b7f982e1aaedfd8d8b975
wundrweapon <rjpappa@gmail.com>
2018-01-29 20:09:30 -0500

    first commit

---
+++ drawing.js
@@ -0,0 +1,55 @@
+function equation(start, cp1, cp2, end, len) {
+	var xt = start.x + " + " + (3 * (cp1.x-start.x)) +"t + " + (3 * (start.x+cp2.x-2*cp1.x)) + "t^2 + " + ((end.x - start.x + 3 * cp1.x - 3 * cp2.x))+ "t^3"
+	$('#xt').text("x(t) = " + xt);
+	var yt = start.y + " + " + (3 * (cp1.y-start.y)) +"t + " + (3 * (start.y+cp2.y-2*cp1.y)) + "t^2 + " + ((end.y - start.y + 3 * cp1.y - 3 * cp2.y))+ "t^3"
+	$('#yt').text("y(t) = " + yt);
+
+	var dx = (3 * (cp1.x-start.x)) +" + " + (6 * (start.x+cp2.x-2*cp1.x)) + "t + " + (3 * (end.x - start.x + 3 * cp1.x - 3 * cp2.x))+ "t^2"
+	var dy = (3 * (cp1.y-start.y)) +" + " + (6 * (start.y+cp2.y-2*cp1.y)) + "t + " + (3 * (end.y - start.y + 3 * cp1.y - 3 * cp2.y))+ "t^2"
+	var dydx = `(${(3 * (cp1.y-start.y)) +" + " + (6 * (start.y+cp2.y-2*cp1.y)) + " * t + " + (3 * (end.y - start.y + 3 * cp1.y - 3 * cp2.y))+ " * Math.pow(t, 2)"})/
+	(${(3 * (cp1.x-start.x)) +" + " + (6 * (start.x+cp2.x-2*cp1.x)) + " * t + " + (3 * (end.x - start.x + 3 * cp1.x - 3 * cp2.x))+ " * Math.pow(t, 2)"})`
+
+	$("#dx").text("dx/dt = " + dx);
+	$('#dy').text("dy/dt = " + dy);
+	$('#len').text("len = " + len);
+	$('#dydx').text(dydx);
+}
+
+$(document).ready(function() {
+	start = {x: 100, y: 25};
+	mid1 = {x: 10, y: 90};
+	mid2 = {x: 110, y: 100};
+	end = {x: 150, y: 195};
+	$('input').change(function() {
+		curve =new Bezier(start , mid1 , mid2 , end);
+		draw(curve);
+		equation(start, mid1, mid2, end, curve.length());
+	});
+	$('#startx').change(function() {
+		start.x = parseFloat($(this).val());
+	});
+	$('#starty').change(function() {
+		start.y = parseFloat($(this).val());
+	});
+
+	$('#mid1x').change(function() {
+		mid1.x = parseFloat($(this).val());
+	});
+	$('#mid1y').change(function() {
+		mid1.y = parseFloat($(this).val());
+	});
+
+	$('#mid2x').change(function() {
+		mid2.x = parseFloat($(this).val());
+	});
+	$('#mid2y').change(function() {
+		mid2.y = parseFloat($(this).val());
+	});
+
+	$('#endx').change(function() {
+		end.x = parseFloat($(this).val());
+	});
+	$('#endy').change(function() {
+		end.y = parseFloat($(this).val());
+	});
+});
